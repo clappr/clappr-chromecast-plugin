ff232c73d0b9c39c6c7d2e97c6cb6f3c8c6ae7fb
Bruno Torres <me@brunotorr.es>
2015-10-14 11:01:32 -0300

    initial plugin implementation

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+var path = require('path');
+var webpack = require('webpack');
+var Clean = require('clean-webpack-plugin');
+
+var plugins = [
+  new webpack.DefinePlugin({
+    VERSION: JSON.stringify(require('./package.json').version)
+  })
+];
+if (process.env.npm_lifecycle_event === 'release') {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {warnings: false},
+    output: {comments: false}
+  }));
+} else {
+  plugins.push(new Clean(['dist']));
+}
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['css', 'sass?includePaths[]='
+            + path.resolve(__dirname, './node_modules/compass-mixins/lib')
+            + '&includePaths[]='
+            + path.resolve(__dirname, './node_modules/clappr/src/base/scss')
+            + '&includePaths[]='
+            + path.resolve(__dirname, './src/base/scss')
+        ],
+        include: path.resolve(__dirname, 'src'),
+      },
+       {
+           test: /\.(png|woff|eot|ttf|swf)/, loader: 'url-loader?limit=1'
+       },
+       {
+           test: /\.svg/, loader: 'file-loader'
+       },
+       {
+           test: /\.html/, loader: 'html?minimize=false'
+       }
+    ],
+  },
+  resolve: {
+    root: path.resolve(__dirname, 'src'),
+    extensions: ['', '.js'],
+  },
+  externals: {
+    "Clappr": "Clappr",
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '<%=baseUrl%>/',
+    filename: 'clappr-chromecast-plugin.js',
+    library: 'Chromecast',
+    libraryTarget: 'umd',
+  },
+};
