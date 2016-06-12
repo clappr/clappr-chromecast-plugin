2318e91b4641e5ae2e99459ef7629dd314380cdf
Bruno Torres <me@brunotorr.es>
2016-06-12 20:29:00 -0300

    add default clappr test page

---
+++ public/j/main.js
@@ -0,0 +1,19 @@
+var Parser = function(output) {
+  this.output = output;
+  this.console = $("#console");
+  this.context = document;
+};
+
+Parser.prototype = {
+  parse: function(code) {
+    try {
+      var old = player;
+      eval(code);
+      old.destroy();
+      window.player = player;
+      this.console.empty();
+    } catch(err) {
+      this.console.html(err.message);
+    }
+  }
+};
